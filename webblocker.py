e035e7b03a5ed6692c045f9652d67d32ca3445d3
Mohit Sharma <44211955+Maddywiz1998@users.noreply.github.com>
2019-10-15 22:59:42 +0530

    Add files via upload
    *Note: PLEASE Provide ADMINISTRATOR RIGHTS BEFORE USING

---
+++ webblocker.py
@@ -0,0 +1,26 @@
+import os
+from time import *  
+from datetime import *   
+host_path = r"C:\Windows\System32\drivers\etc\hosts"
+redirect = "127.0.0.1"  
+websites = ["www.facebook.com", "https://www.facebook.com"]
+os.chmod(host_path,0o777)
+
+while True:  
+    if datetime(datetime.now().year,datetime.now().month,datetime.now().day,22)<datetime.now()<datetime(datetime.now().year,datetime.now().month,datetime.now().day,23):  
+        with open(host_path,"r+") as fileptr:  
+            content = fileptr.read()  
+            for website in websites:  
+                if website in content:  
+                    pass  
+                else:  
+                    fileptr.write(redirect+"        "+website+"\n")  
+    else:  
+        with open(host_path,'r+') as file:  
+            content = file.readlines();  
+            file.seek(0)  
+            for line in content:  
+                if not any(website in line for website in               websites):  
+                    file.write(line)  
+            file.truncate()  
+    sleep(5)  
